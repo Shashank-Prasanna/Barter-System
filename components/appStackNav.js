d02317acfedfbb5ee7c29fe020f8716efcfea33b
Shashank Prasanna <shashankprasanna1@gmail.com>
2021-08-24 17:11:31 -0400

    Made changes to fix the navigation structure (had infinite loops)

--- components/appStackNav.js
+++ components/appStackNav.js
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import { TabNav } from './tabNav'
+import {AppDrawerNav, appDrawerNav} from './appDrawerNav';
 import UserDetailsScreen from '../screens/userDetailsScreen';
 import {createStackNavigator} from 'react-navigation-stack'
 
 export const AppStackNavigator = createStackNavigator({
-    TabNav: {screen: TabNav, navigationOptions:{headerShown: false}},
-    UserDetails: {screen: UserDetailsScreen, navigationOptions: {headerShown: false}},
+    AppDrawer: {screen: AppDrawerNav, navigationOptions:{headerShown: false}},
+    UserDetails: {screen: UserDetailsScreen, navigationOptions: {headerShown: false}}
 })
