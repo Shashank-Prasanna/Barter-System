1fc9aab39e8f180178b2839f87b9cef7701353d3
Shashank Prasanna <shashankprasanna1@gmail.com>
2021-08-27 11:42:01 -0400

    Added sendItem and sendNotification function

--- screens/myBartersScreen.js
+++ screens/myBartersScreen.js
@@ -10,7 +10,9 @@ export default class MyBarterScreen extends React.Component {
     constructor(){
         super()
         this.state = {
-            bartersList: []
+            bartersList: [],
+            email: firebase.auth().currentUser.email,
+            username: ''
         }
     }    
 
@@ -22,7 +24,37 @@ export default class MyBarterScreen extends React.Component {
         console.log(this.state.bartersList)
     }
 
+    sendNotification = async(item) => {
+        let doc_ID = ' '
+        await db.collection('notifications').where('user_ID', '==', item.exchanger_email).where('exchange_ID', '==', item.exchange_ID).get()
+        .then(snapshot => {
+            snapshot.forEach(doc => {
+                doc_ID = doc.id
+            })
+        })
+        console.log(doc_ID)
+        db.collection('notifications').doc(doc_ID).update({
+            message: this.state.username + ' has sent the item',
+        })
+    }
+
+    sendItem = (item) => {
+        console.log(item)
+        this.sendNotification(item)
+    }
+
+    getUserInfo = () => {
+        db.collection('users').where('email', '==', this.state.email).get().then((snapshot) => {
+            snapshot.forEach((doc) => {
+                this.setState({
+                    username: doc.data().username
+                })
+            })
+        })
+    }
+
     componentDidMount() {
+        this.getUserInfo()
         this.getAllBarters()
     }
 
@@ -38,7 +70,9 @@ export default class MyBarterScreen extends React.Component {
                 titleStyle={{color: '#ff681dff', fontWeight: 'bold', fontSize: 20}}
                 style={{width: '100%'}}
                 rightElement={
-                    <TouchableOpacity style={{backgroundColor: '#349b9aff', paddingHorizontal: 40, paddingVertical: 10}}>
+                    <TouchableOpacity style={{backgroundColor: '#349b9aff', paddingHorizontal: 40, paddingVertical: 10}} onPress={() => {
+                        this.sendItem(item)
+                    }}>
                         <Text style={{fontSize: 18}}>Barter</Text>
                     </TouchableOpacity>
                 }
