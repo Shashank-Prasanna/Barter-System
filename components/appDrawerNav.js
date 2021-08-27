18c7730a56ce6e7858d1c1a6d15b913622bb7948
Shashank Prasanna <shashankprasanna1@gmail.com>
2021-08-27 11:41:18 -0400

    Added notifications screen to the App Drawer

--- components/appDrawerNav.js
+++ components/appDrawerNav.js
@@ -1,11 +1,8 @@
 import * as React from 'react';
-import { Text, View, StyleSheet} from 'react-native';
-import {SafeAreaProvider} from 'react-native-safe-area-context'
-import {createAppContainer, createSwitchNavigator} from 'react-navigation'
-import {createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
 import {createDrawerNavigator} from 'react-navigation-drawer'
 import SideBar from '../components/sideBar.js'
 import SettingsScreen from '../screens/settingsScreen' 
+import NotificationsScreen from '../screens/notificationsScreen.js';
 import { TabNav } from '../components/tabNav'
 import MyBartersScreen from '../screens/myBartersScreen'
 
@@ -13,14 +10,17 @@ export const AppDrawerNav = createDrawerNavigator({
   Home:{
     screen: TabNav,
   },
-  Settings:{
-    screen: SettingsScreen,
-  },
   MyBarters:{
     screen: MyBartersScreen,
     navigationOptions: {
       title: 'My Barters',
     }
+  },
+  Notifications:{
+    screen: NotificationsScreen,
+  },
+  Settings:{
+    screen: SettingsScreen,
   },
 },
 {
